36773f759cdf2fc67a28ee9f521e56306f6accf8
aa <aac1291@gmx.ch>
2023-01-26 12:14:48 +0100

    css, assets

--- ClientApp/webpack.config.js
+++ ClientApp/webpack.config.js
@@ -2,11 +2,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const { env } = require("process");
 
 module.exports = {
   devtool: env.production ? "none" : "source-map",
-  entry: "./src/index.ts",
+  entry: { 
+    main: "./src/index.ts", 
+    // chunkxy: "./src/about.ts" 
+  },
   output: {
     path: path.resolve(__dirname, "../wwwroot"),
     filename: "[name].[chunkhash].js",
@@ -25,15 +29,30 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+      { // this loader is required when using CopyPlugin
+        test: /\.(png|jpg|gif)$/,   
+        type: 'asset/resource'
+      }    
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({             // default => inject 'all' chunks
       template: "./src/index.html",
+      filename: "index.html"
     }),
+    // new HtmlWebpackPlugin({          // add more plugin instances 
+    //   chunks: [ "chunkxy"],          // to inject specific chunk's    
+    //   template: "./src/about.html",  // to specific file
+    //   filename: "about.html"
+    // }),
     new MiniCssExtractPlugin({
       filename: "css/[name].[chunkhash].css",
+    }),
+    new CopyPlugin({ // to copy existing resources to output, requires loader type: 'asset/resource'
+      patterns: [
+        { from: "src/assets/*.*" , to: "assets/[name][ext]" },
+      ],
     }),
   ],
 };
